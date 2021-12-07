b00a29070f1dd6544180acd5f9ea39a68c085f48
Samuli Immonen <samuli.immonen@tuni.fi>
2021-12-07 11:25:30 +0200

    tried to fix

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const port = process.env.PORT || 8080;
 
 const db = [{ name: "tiina" }, { name: "jack" }];
 
-const server = app.listen(port, () => {
-  console.log(`Listening on port ${server.address().port}`);
-});
-
 app.get("/names", (req, res) => {
   res.send(db);
+});
+
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${server.address().port}`);
 });
