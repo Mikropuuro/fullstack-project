1c38a817d7a98852d0eba9b0eb03b022c2a88faa
Samuli Immonen <samuli.immonen@tuni.fi>
2022-01-12 17:42:18 +0200

    added answer-check-feature

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import WordFinnish from "./word.js";
+
+function App() {
+  return (
+    <div>
+      <h1>Try to guess the word in English</h1>
+      <WordFinnish />
+    </div>
+  );
+}
+
+export default App;
