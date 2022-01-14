de8d6c8a90660a8ff29e5e4540fba26dd9d98c56
Samuli Immonen <samuli.immonen@tuni.fi>
2022-01-14 19:33:32 +0200

    added app.js

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import React from "react";
+import Nav from "./nav.js";
+
+/**
+ *
+ * @returns Nav-component which renders the other components
+ */
+
+function App() {
+  return (
+    <div className="navbar">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
